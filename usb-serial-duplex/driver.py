e2057df110c1afd7d6ee9bdcab8fe0288b79b895
Jukka Dahlbom <jukka.dahlbom@zebit.fi>
2015-11-24 23:54:29 +0200

    Separator search successful

--- usb-serial-duplex/driver.py
+++ usb-serial-duplex/driver.py
@@ -5,37 +5,52 @@ import time
 port = sys.argv[1]
 baudrate = 9600
 
-FIRST_BYTE='s'
-LAST_BYTE='w'
 
 constant_data = ''.join(chr(x) for x in [0x71, 0x72, 0x73, 0x74, 0x75, 0x76,0x77,0x78,0x79,0x60,0x61,0x62,0x63,0x64,0x65,0x66])
 
 print "Using port: "+ port
 conn = serial.Serial(port, baudrate)
 if (not conn.isOpen()):
-	conn.open()
+    conn.open()
+
+buf = []
 
 def print16bytes(data):
-	for val in data:
-		print format(ord(val),"x") + " ",
-	print ""
+    for val in data:
+        print format(ord(val),"x") + " ",
+    print ""
+
+def findMessageSeparator(buf):
+    separatorFound = False
+    while not separatorFound:
+        if conn.inWaiting() > 0:
+            print str(conn.inWaiting()) + " bytes available"
+            bytesread = conn.read(conn.inWaiting())
+            buf.extend(bytesread)
+            try:
+                zeroindex = buf.index(chr(0))
+                print "found zero index: "+ str(zeroindex)
+                print "Before: Length of buffer: "+ str(len(buf))
+                buf = buf[zeroindex:]
+                print "After:  Length of buffer: "+ str(len(buf))
+                if len(buf) > 1:
+                    print "index 1: "+ str(ord(buf[1]))
+                    if ord(buf[1]) is 0:
+                        separatorFound = True
+                    else:
+                        print "Discarding first two bytes: " + str(ord(buf[0])) +", " + str(ord(buf[1]))
+                        buf.pop(2)
+            except:
+                print "No zero bytes, discarded buffer"
+                return []
+    print "Separator found"
+    return buf
+
+# Receive "start of transmission" bytes
+# Loop and keep receiving TLV-frames ad infinitum.
+# Read TL + correct number of bytes for V until done.
 
 while True:
-	if conn.inWaiting() > 0:
-		startbyte = conn.read()
-		if startbyte == FIRST_BYTE:
-			controlbytes = conn.read(16)
-			print "Receiving: ",
-			print16bytes(controlbytes)
-			startbyte = conn.read()
-		if startbyte != LAST_BYTE:
-			print 'invalid WAIT byte: '+ startbyte
-			conn.flushInput()
-		else :
-			conn.write(FIRST_BYTE)
-			print "Transmitting: ",
-			print16bytes(constant_data)
-			conn.write(constant_data)
-		conn.write(LAST_BYTE)
-	time.sleep(0.03)
+    buf = findMessageSeparator(buf)
+    time.sleep(0.5)
 
